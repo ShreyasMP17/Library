df45fd89bff9eeb1c5ab12af10fd1d7e8eca2e88
ShreyasMP17 <shreyasmp.7@gmail.com>
2023-07-18 14:44:37 +0530

    first commit

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -9,10 +9,10 @@ const LandingPage = () => {
             <h1>LIBRARY MANAGEMENT SYSTEM</h1>
             <div className="selectLogin">
                 <div className="adm"> 
-                <img height="130" width="130" src="./images/lg4.png" alt="" /> <br />
+                <img height="130" width="230" src="./images/lg4.png" alt="" /> <br />
                 <Link to="/admin-login" id="one">Admin Login</Link></div>
                 <div className="use">
-                <img height="130" width="130" src="./images/lg4.png" alt="" /><br />
+                <img height="130" width="230" src="./images/lg4.png" alt="" /><br />
                 <Link to="/user-login"id="two">User Login</Link></div>
                 </div> 
                  </div>
